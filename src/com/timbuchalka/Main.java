a3ae8ce40934b55f723ec3413e70818e81ada629
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-17 11:05:51 +0200

    expressions

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,27 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+//        expressions();
+        statementsWhitespacesAndIndentation();
+    }
+
+    private static void expressions() {
+        // keywords double, int, class etc
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("this is an expression");
+        }
+
+        int score = 100;
+        if(score > 99) {
+            System.out.println("You got the high score");
+            score = 0;
+        }
+    }
+
+    private static void statementsWhitespacesAndIndentation() {
+
     }
 }
