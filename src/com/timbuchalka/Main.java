e88cdb40f256ee4fb205d86b7416fa4aef448c43
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-17 11:19:46 +0200

    statements, whitespaces, indentiation (wciecia)

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -17,13 +17,23 @@ public class Main {
         }
 
         int score = 100;
-        if(score > 99) {
+        if (score > 99) {
             System.out.println("You got the high score");
             score = 0;
         }
     }
 
     private static void statementsWhitespacesAndIndentation() {
+        int myVariable = 50; // myVariable = 50 -> expression, int myVariable = 50 -> statement
+        myVariable++;
+        myVariable--;
 
+        System.out.println("this is a test");
+        System.out.println("this is " +
+                "another " +
+                "test");
+
+        int anotherVariable = 50;
+        myVariable--;
     }
 }
