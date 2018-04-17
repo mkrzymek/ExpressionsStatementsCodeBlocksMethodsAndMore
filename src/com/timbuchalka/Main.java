ca21b48469a4adfd87839a1819ca41a51bd2e0c3
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-17 11:38:07 +0200

    Code Blocks And The If Then Else Control Statements

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 //        expressions();
-        statementsWhitespacesAndIndentation();
+//        statementsWhitespacesAndIndentation();
+        CodeBlocksAndTheIfThenElseControlStatements();
     }
 
     private static void expressions() {
@@ -35,5 +36,29 @@ public class Main {
 
         int anotherVariable = 50;
         myVariable--;
+    }
+
+    private static void CodeBlocksAndTheIfThenElseControlStatements() {
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+//        if(score < 5000 && score > 1000) {
+//            System.out.println("your score is 5000");
+//        } else if (score < 1000) {
+//            System.out.println("bla bla");
+//        } else {
+//            System.out.println("Got here");
+//        }
+
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("final score " + finalScore);
+        }
+
+
+
+
     }
 }
